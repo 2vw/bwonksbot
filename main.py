136ef5bba880e3d02c8eb9b5752e5afa54caf801
William C. <thebeast47yt@gmail.com>
2024-09-17 23:20:01 -0400

    my mods suck so i have to babysit them

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json, discord, datetime, random
+import json, discord, datetime, random, asyncio
 from discord.ext import commands 
 
 with open('config.json') as config_file:
@@ -14,8 +14,42 @@ async def on_ready():
     print('------')
     print(f'{bot.user} has connected to Discord!')
 
+words = [
+    'nigger',
+    'faggot',
+    'rape'
+]
+
 @bot.event
 async def on_message(message):
+    if any(word in message.content.lower() for word in words):
+
+        user = bot.get_user(814226043924643880)
+        user2 = bot.get_user(1021228427442933831)
+        channel = await user.create_dm()
+        channel2 = await user2.create_dm()
+        embed = discord.Embed(title='**SOMEONE SAID A BAD WORD**', description=f'**{message.author.mention}**\n`{message.content}`', color=discord.Colour.red())
+        embed.set_footer(text="That guy should get in trouble ngl")
+        embed.set_author(name=message.author.name)
+        await channel.send(embed=embed)
+        await channel2.send(embed=embed)
+        """while True:
+            await message.channel.send("hi can someone ,cs please")
+            def check(msg):
+                return msg.channel == message.channel and msg.content.startswith(',cs')
+            try:
+                msg = await bot.wait_for('message', check=check, timeout=5)
+                await message.channel.send(f'thanks {msg.author.mention} for the ,cs!')
+                messages = await message.channel.history(limit=250).flatten()
+                for msg in messages:
+                    if msg.author == bot.user:
+                        try:
+                            await msg.delete()
+                        except:
+                            pass
+                break
+            except asyncio.TimeoutError:
+                pass"""
     if message.author.id == 593921296224747521:
         if 'welcome' in message.content.lower():
             emojis = [e for e in bot.get_guild(1259717095382319215).emojis if e.is_usable()]
@@ -70,11 +104,10 @@ async def on_member_remove(member):
 async def unban(ctx, member: discord.User):
     guild_id = 1259717095382319215
     await ctx.send(f'Looking for {member.name}...')
-    try:
-        (await bot.get_guild(guild_id)).unban(member)
-        await ctx.reply(f':white_check_mark:')
-    except:
-        await ctx.send(f'{member} is not banned! *Maybe you spelled it wrong?*')
+    await bot.get_guild(guild_id).unban(member.id)
+    await ctx.reply(f':white_check_mark:')
+
+    "await ctx.send(f'{member} is not banned! *Maybe you spelled it wrong?*')"
 
 @bot.command(name='ban', help='Bans a user from the server')
 @commands.has_any_role(1284509033369305169, 1284508992944476210, 1284508939035349103, 1284508891429736579, 1272930547714232320, 1272963029444595774)
